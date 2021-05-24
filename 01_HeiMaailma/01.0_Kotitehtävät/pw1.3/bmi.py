0501434087684bc0df704fdc411ecf55e690f6c9
Sami Kulonpää <bgh296@myy.haaga-helia.fi>
2021-05-24 20:05:03 +0300

    edit dirname

--- 01_HeiMaailma/01.0_Kotitehtävät/pw1.3/bmi.py
+++
@@ -1,9 +0,0 @@
-def laske_bmi(pituus, paino):
-    bmi = paino / (pituus*pituus)
-    print("Painoindeksisi on")
-    print(round(bmi, 2))	
-
-pituus = float(input("Syötä pituutesi: "))
-paino = float(input("Syötä painosi: "))
-
-laske_bmi(pituus, paino)
--- 01_HeiMaailma/01.0_Kotitehtävät/pw1.3/moikkailija.py
+++
@@ -1,3 +0,0 @@
-firstname = input("Kerro etunimesi: ")
-lastname = input("Kerro sukunimesi: ")
-print("Moikka " + firstname + " "  + lastname + "!")
