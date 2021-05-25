8e4e85b032d85f81744139a5cda33f73c5fb9b82
Sami Kulonpää <bgh296@myy.haaga-helia.fi>
2021-05-25 14:03:57 +0300

    change allowed methods

--- 002-templates-and-forms/205-template-with-forms/hello.py
+++ 002-templates-and-forms/205-template-with-forms/hello.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 app = Flask("__name__")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def index():
 	return render_template("index.html", title="index")
 
-@app.route("/foo", methods=["GET", "POST"])
+@app.route("/foo", methods=["GET"])
 def foo():
 	return render_template("foo.html", title="foo")
 
-@app.route("/bar", methods=["GET", "POST"])
+@app.route("/bar", methods=["POST"])
 def bar():
 	return render_template("bar.html", title="bar")
 
