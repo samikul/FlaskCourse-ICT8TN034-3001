1b7f06dc7799f734341a80dfa06b320b376dbe37
Sami Kulonpää <bgh296@myy.haaga-helia.fi>
2021-05-25 14:54:24 +0300

    add different form types with post method

--- 002-templates-and-forms/205-template-with-forms/hello.py
+++ 002-templates-and-forms/205-template-with-forms/hello.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 app = Flask("__name__")
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST", "GET"])
 def index():
 	return render_template("index.html", title="index")
 
-@app.route("/foo", methods=["GET"])
+@app.route("/foo", methods=["POST", "GET"])
 def foo():
 	return render_template("foo.html", title="foo")
 
-@app.route("/bar", methods=["POST"])
+@app.route("/bar", methods=["POST", "GET"])
 def bar():
 	return render_template("bar.html", title="bar")
 
